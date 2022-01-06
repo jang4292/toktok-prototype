41a5284190a78c8d4635984f7088368e3d2ff86e
jangyunhwan <jangyunhwan@gmail.com>
2022-01-06 23:43:45 +0900

    Update project to be successful build

--- app/src/main/java/com/example/toktok/ui/search/SearchFragment.kt
+++ app/src/main/java/com/example/toktok/ui/search/SearchFragment.kt
@@ -1,9 +1,9 @@
 package com.example.toktok.ui.search
 
-import android.arch.lifecycle.Observer
-import android.arch.lifecycle.ViewModelProvider
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import android.support.v4.app.Fragment
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
