3aaec8978252b22a7ef0222fc316ce29755009fd
jangyunhwan <jangyunhwan@gmail.com>
2022-01-07 15:37:47 +0900

    Update info fragment

--- app/src/main/java/com/example/toktok/ui/info/InfoFragment.kt
+++ app/src/main/java/com/example/toktok/ui/info/InfoFragment.kt
@@ -1,41 +1,46 @@
 package com.example.toktok.ui.info
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.example.toktok.MainActivity
 import com.example.toktok.databinding.FragmentInfoBinding
 
 class InfoFragment : Fragment() {
 
-    private lateinit var infoViewModel: InfoViewModel
     private var _binding: FragmentInfoBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
 
+    lateinit var mainActivity: MainActivity
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mainActivity = context as MainActivity
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        infoViewModel =
-            ViewModelProvider(this, ViewModelProvider.NewInstanceFactory()).get(
-                InfoViewModel::class.java
-            )
-
         _binding = FragmentInfoBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textInfo
-        infoViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+        val llLoginLinearLayout = binding.llLoginLinearLayout
+        val llLoginDetailLinearLayout = binding.llLoginDetailLinearLayout
+
+        val loginButton = binding.btnInfoLogin
+        loginButton.setOnClickListener {
+            llLoginLinearLayout.visibility = View.GONE
+            llLoginDetailLinearLayout.visibility = View.VISIBLE
+        }
+
         return root
     }
 
