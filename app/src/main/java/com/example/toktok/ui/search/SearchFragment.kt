41bb54852173fd516a75a60947156a5f7e0c15fe
jangyunhwan <jangyunhwan@gmail.com>
2022-02-27 13:44:20 +0900

    Modify search fragment to add edit text

--- app/src/main/java/com/example/toktok/ui/search/SearchFragment.kt
+++ app/src/main/java/com/example/toktok/ui/search/SearchFragment.kt
@@ -1,18 +1,14 @@
 package com.example.toktok.ui.search
 
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.fragment.app.Fragment
 import com.example.toktok.databinding.FragmentSearchBinding
 
 class SearchFragment : Fragment() {
 
-    private lateinit var searchViewModel: SearchViewModel
     private var _binding: FragmentSearchBinding? = null
 
     // This property is only valid between onCreateView and
@@ -24,19 +20,8 @@ class SearchFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        searchViewModel =
-            ViewModelProvider(
-                this,
-                ViewModelProvider.NewInstanceFactory()
-            ).get(SearchViewModel::class.java)
-
         _binding = FragmentSearchBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
-        val textView: TextView = binding.textSearch
-        searchViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
         return root
     }
 
