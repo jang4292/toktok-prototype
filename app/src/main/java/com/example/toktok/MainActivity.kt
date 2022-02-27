850f0ee7c20130dfdd5c8d633d9f96d6e0f98206
jangyunhwan <jangyunhwan@gmail.com>
2022-02-27 13:43:08 +0900

    Hide actionbar

--- app/src/main/java/com/example/toktok/MainActivity.kt
+++ app/src/main/java/com/example/toktok/MainActivity.kt
@@ -35,5 +35,8 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        supportActionBar?.hide()
+
     }
 }
