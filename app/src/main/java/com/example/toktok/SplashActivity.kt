3f6414b74d422416442d0da7d628794c12a1ddf6
jangyunhwan <jangyunhwan@gmail.com>
2022-04-19 23:42:38 +0900

    Modify flag set about splash activity after checking button #3

--- app/src/main/java/com/example/toktok/SplashActivity.kt
+++ app/src/main/java/com/example/toktok/SplashActivity.kt
@@ -46,7 +46,6 @@ class SplashActivity : AppCompatActivity() {
                 val intent = Intent(this, MainActivity::class.java)
                 intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
                 startActivity(intent)
-                finish()
             }
         }
     }
@@ -59,6 +58,7 @@ class SplashActivity : AppCompatActivity() {
         when (requestCode) {
             1 -> {
                 val intent = Intent(this, MainActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
                 startActivity(intent)
 
                 // If request is cancelled, the result arrays are empty.
